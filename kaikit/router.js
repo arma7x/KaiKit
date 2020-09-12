5ed54fafcb98d7af3c30a4f381b22916a10d4392
arma7x <arma7x@live.com>
2020-09-12 12:45:53 +0800

    fix softkey style

--- kaikit/router.js
+++ kaikit/router.js
@@ -360,7 +360,7 @@ const KaiRouter = (function() {
           right: ''
         },
         mustache: Mustache,
-        template: '<div kai:click="clickLeft()" style="width:32%;text-align:left;padding-left:5px;font-weight:bold;">{{ left }}</div><div kai:click="clickCenter()" style="width:36%;text-align:center;font-weight:800;">{{ center }}</div><div kai:click="clickRight()" style="width:32%;text-align:right;padding-right:5px;font-weight:bold;">{{ right }}</div>',
+        template: '<div kai:click="clickLeft()" style="width:32%;text-align:left;padding-left:5px;font-weight:600;font-size:14px;">{{ left }}</div><div kai:click="clickCenter()" style="width:36%;text-align:center;font-weight:800;">{{ center }}</div><div kai:click="clickRight()" style="width:32%;text-align:right;padding-right:5px;font-weight:600;font-size:14px;">{{ right }}</div>',
         mounted: function() {
           EL.style.height = '30px';
           EL.style.lineHeight = '30px';
